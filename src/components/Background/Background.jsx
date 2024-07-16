203c6d897a95dd38bb96f3719da1ab7c1c6613e5
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-16 23:55:42 +0200

    added component for background

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Background.scss';
+
+const Background = () => {
+  return (
+    <div className="background">
+      <div className="grid"></div>
+    </div>
+  );
+};
+
+export default Background;
