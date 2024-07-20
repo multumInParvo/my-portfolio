5450eec04196dc817f98288f1b46621c91d5f55c
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-20 17:48:11 +0200

    separated concerns for better modularity and maintainability

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -10,16 +10,7 @@ function Contact() {
             <Helmet>
                 <title>CONTACT | oleksandr pryvalov</title>
             </Helmet>
-            <div className='contact-container'>
-                <h1 className='contact-title'>CONTACT</h1>
-                <div className='info-container'>
-                    <div className='contact-info'>
-                        <span className='contact-detail-title'>Ask me anything here or send an email directly to</span>
-                        <span className='contact-detail'>oleksandrpryv@gmail.com</span>
-                    </div>
-                    <Form />
-                </div>
-            </div>
+            <Form />
         </HelmetProvider>
     );
 }
