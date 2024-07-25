9607f8f808cc75bbd710b03e18e2dfa4de3c61c8
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-25 13:21:16 +0200

    implemented responsive masonry and added projectcard component

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import projectsData from '../../data/projects-data.json';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
-import '../Projects/Projects.scss';
+import ProjectModal from '../../components/ProjectModal/ProjectModal';
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import './Projects.scss';
 
 function Projects() {
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = (project) => {
+    setSelectedProject(project);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setTimeout(() => setSelectedProject(null), 300); // Wait for the closing animation
+  };
+
   return (
     <HelmetProvider>
-      <div className='projects-title-container'>
+      <div className='projects'>
         <h1 className='menu-section-title'>PROJECTS</h1>
         <Helmet>
           <title>Projects | Oleksandr Pryvalov</title>
         </Helmet>
         <div className="projects-container">
-          {projectsData.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))}
+          <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 1, 1100: 2 }}>
+            <Masonry gutter="20px">
+              {projectsData.map((project, index) => (
+                <ProjectCard 
+                  key={index} 
+                  project={project} 
+                  onClick={openModal}
+                />
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
         </div>
+        <ProjectModal 
+          project={selectedProject} 
+          isOpen={isModalOpen} 
+          onClose={closeModal}
+        />
       </div>
     </HelmetProvider>
   );
