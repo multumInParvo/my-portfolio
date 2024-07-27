f8a07932b54f62a72b2cee32a752b9045a32b6a6
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-27 18:03:18 +0200

    updated component

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -5,6 +5,7 @@ import '../Introduction/Introduction.scss';
 function Introduction() {
     return (
         <div className="introduction-container">
+            <h2 className='introduction-title'>I'm Oleksandr Pryvalov</h2>
             <div className='introduction-text'>
                 <p className='introduction-paragraph'>
                     A front-end developer with an interest in coding and art, appreciating minimalism in both fields.
