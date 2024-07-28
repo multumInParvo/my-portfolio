31ad564976d0b38a1f114acf768b2fcef33b021f
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-28 23:42:02 +0200

    added image to introduction component

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,14 +1,20 @@
 // Introduction //
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../Introduction/Introduction.scss';
 
 function Introduction() {
     return (
         <div className="introduction-container">
-            <h2 className='introduction-title'>I'm Oleksandr Pryvalov</h2>
+            <div className="title-avatar-container">
+                <h2 className='introduction-title'>I'm Oleksandr Pryvalov</h2>
+                <div className='avatar-container'>
+                    <img src="/images/avatar.jpeg" alt="Oleksandr Pryvalov" className="avatar" />
+                </div>
+            </div>
             <div className='introduction-text'>
                 <p className='introduction-paragraph'>
-                    A front-end developer with an interest in coding and art, appreciating minimalism in both fields.
+                    A front-end developer with an interest in coding and art, appreciating clarity in both fields.
                     I'm looking for projects where I can apply what I've learned and continue to develop my skills.
                 </p>
                 <p className='introduction-paragraph'>
@@ -16,8 +22,8 @@ function Introduction() {
                     These two interests help me balance logic with creativity.
                 </p>
                 <p className='introduction-paragraph'>
-                    I recently finished my training as a web integrator at OpenClassrooms, equipping me with the fundamentals in front-end development.
-                    Take a look at my projects to see what I've worked on so far.
+                    I'm finishing my training as a web integrator at OpenClassrooms, equipping myself with the fundamentals in front-end development.
+                    Take a look at my <Link to="/projects" className="projects-link">projects</Link> to see what I've worked on so far.
                 </p>
             </div>
         </div>
