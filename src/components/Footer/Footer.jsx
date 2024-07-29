e76672591421cf878ecdb9d1a3a2fb0b79067bc5
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-29 18:41:19 +0200

    import fontawesome library, add icons for social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,23 @@
 // Footer
 import React from 'react';
 import '../Footer/Footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
-        <div className="footer">
-            <div className='footer-text'>
-                <p className='footer-logo'>
-                    <span>© 2024 by Oleksandr Pryvalov</span>
-                </p>
+        <div className='footer'>
+            <div className='social-links'>
+                <a href="https://github.com/multumInParvo" target="_blank" rel="noopener noreferrer" className="social-link">
+                    <FontAwesomeIcon icon={faGithub} className="github-icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/oleksandr-pryvalov-bb0599298" target="_blank" rel="noopener noreferrer" className="social-link">
+                    <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+                </a>
             </div>
         </div>
     );
 }
 
-export default Footer;
+export default Footer; 
+
