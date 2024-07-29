4eb2504253bfa4280007fb2a87ece7000e480ac2
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-29 18:57:17 +0200

    Update components to use translated text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App //
 import { Routes, Route } from 'react-router-dom';
-import '../src/styles/Global.scss';
 import React from 'react';
+import { LanguageProvider } from './context/LanguageContext';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomeAbout from './pages/HomeAbout/HomeAbout';
@@ -9,22 +9,25 @@ import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
 import Cursor from './components/Cursor/Cursor';
 import Background from './components/Background/Background';
+import '../src/styles/Global.scss';
 
 function App() {
   return (
-    <div className="app">
-      <Background />
-      <Cursor />
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomeAbout />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <LanguageProvider>
+      <div className="app">
+        <Background />
+        <Cursor />
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeAbout />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </LanguageProvider>
   );
 }
 
--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,25 +1,27 @@
 // Introduction //
-import React from 'react';
+import React, { useContext } from 'react';
 import {Link} from 'react-router-dom';
+import { LanguageContext } from '../../context/LanguageContext';
 import '../Introduction/Introduction.scss';
 
 function Introduction() {
+    const { language, translations } = useContext(LanguageContext);
+    const t = translations[language].introduction;
+
     return (
         <div className="introduction-container">
             <div className="title-avatar-container">
-                <h2 className='introduction-title'>I'm Oleksandr Pryvalov</h2>
+                <h2 className='introduction-title'>{t.title}</h2>
                 <div className='avatar-container'>
                     <img src="/images/avatar.jpeg" alt="Oleksandr Pryvalov" className="avatar" />
                 </div>
             </div>
             <div className='introduction-text'>
                 <p className='introduction-paragraph'>
-                    A front-end developer with an interest in coding and art, appreciating clarity in both fields.
-                    I'm looking for projects where I can apply what I've learned and continue to develop my skills.
+                {t.paragraph1}
                 </p>
                 <p className='introduction-paragraph'>
-                    When I'm not coding, I enjoy creating oil paintings.
-                    These two interests help me balance logic with creativity.
+                {t.paragraph2}
                 </p>
                 <p className='introduction-paragraph'>
                     I'm finishing my training as a web integrator at OpenClassrooms, equipping myself with the fundamentals in front-end development.
