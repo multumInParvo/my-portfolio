56c951829ba78bc7158effac1764405b61353b95
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-07-29 18:55:43 +0200

    implement LanguageSwitcher component

---
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -0,0 +1,21 @@
+// src/components/LanguageSwitcher/LanguageSwitcher.jsx
+
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../context/LanguageContext';
+import './LanguageSwitcher.scss';
+
+function LanguageSwitcher() {
+  const { language, setLanguage } = useContext(LanguageContext);
+
+  const toggleLanguage = () => {
+    setLanguage(language === 'en' ? 'fr' : 'en');
+  };
+
+  return (
+    <button className="language-switcher" onClick={toggleLanguage}>
+      {language === 'en' ? 'FR' : 'EN'}
+    </button>
+  );
+}
+
+export default LanguageSwitcher;
