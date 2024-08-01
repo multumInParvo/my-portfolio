15461967964b3900c41eacf5b23c8c21519f99b8
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-08-01 22:27:03 +0200

    Add ThemeProvider at App level to toggle between light and dark themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import { LanguageProvider } from './context/LanguageContext';
+import { ThemeProvider } from './context/ThemeContext';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomeAbout from './pages/HomeAbout/HomeAbout';
@@ -9,24 +10,27 @@ import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
 import Cursor from './components/Cursor/Cursor';
 import Background from './components/Background/Background';
+import './styles/Theme.scss'
 import '../src/styles/Global.scss';
 
 function App() {
   return (
     <LanguageProvider>
-      <div className="app">
-        <Background />
-        <Cursor />
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomeAbout />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <ThemeProvider>
+        <div className="app">
+          <Background />
+          <Cursor />
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<HomeAbout />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </ThemeProvider>
     </LanguageProvider>
   );
 }
