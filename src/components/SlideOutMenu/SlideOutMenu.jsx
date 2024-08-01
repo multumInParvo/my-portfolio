b3152ea648f4478ee0059b5d5c26d726fac7634d
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-08-01 23:27:38 +0200

    Import useTheme, ThemeToggle component/add LanguageSwitcher and ThemeToggle

--- src/components/SlideOutMenu/SlideOutMenu.jsx
+++ src/components/SlideOutMenu/SlideOutMenu.jsx
@@ -1,10 +1,13 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
+import { useTheme } from '../../context/ThemeContext';
 import { Link } from 'react-router-dom';
 import { LanguageContext } from '../../context/LanguageContext';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
+import ThemeToggle from '../ThemeToggle/ThemeToggle';
 import '../SlideOutMenu/SlideOutMenu.scss';
 
 function SlideOutMenu({ isOpen, onClose }) {
+    useTheme();
     const { language, translations } = useContext(LanguageContext);
     const t = translations[language].slideOutMenu;
 
@@ -20,13 +23,13 @@ function SlideOutMenu({ isOpen, onClose }) {
                     <div className="grid"></div>
                 </div>
                 <nav aria-label="Mobile Navigation">
-                    <Link to="/" className='home-link' onClick={onClose} aria-label="Home">
-                        <span className='asterisk-symbol' aria-hidden="true">✣</span>
-                        <span className="visually-hidden">Home</span>
-                    </Link>
+                    <Link to="/" className='menu-links' onClick={onClose}>{t.home}</Link>
                     <Link to="/projects" className='menu-links' onClick={onClose}>{t.projects}</Link>
                     <Link to="/contact" className='menu-links' onClick={onClose}>CONTACT</Link>
-                    <LanguageSwitcher onClick={onClose}/>
+                    <div className='toggles-wrapper'>
+                        <LanguageSwitcher onClick={onClose} />
+                        <ThemeToggle onClick={onClose} />
+                    </div>
                 </nav>
             </div>
             {isOpen && <div className="overlay" onClick={onClose} aria-hidden="true"></div>}
