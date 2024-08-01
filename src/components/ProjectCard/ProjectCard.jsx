99e82b4c3e422d735f47a8b1bb0c300635d35c0a
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-08-01 23:12:23 +0200

    Import LanguageContext

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../context/LanguageContext';
 import './ProjectCard.scss';
 
 const ProjectCard = ({ project, onClick }) => {
+  const { language, translations } = useContext(LanguageContext);
+  const t = translations[language].projectCard;
+
   return (
     <div className="project-card" onClick={() => onClick(project)}>
       <img src={project.picture} alt={project.title} className="project-image" />
       <div className="project-overlay">
         <h3 className="project-title">{project.title}</h3>
-        <p className="project-description">{project.description}</p>
+        <p className="project-description">{translations[language].projectDescriptions[project.description]}</p>
         <div className="project-technologies">
           {project.technologies && project.technologies.map((tech, index) => (
             <span key={index} className="tech-tag">{tech}</span>
