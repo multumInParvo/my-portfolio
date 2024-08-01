d3124cf9bcbaddafc4b992580b419b71423aabcf
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-08-01 23:17:08 +0200

    Import LanguageContext and wrap Introduction component in div

--- src/pages/HomeAbout/HomeAbout.jsx
+++ src/pages/HomeAbout/HomeAbout.jsx
@@ -1,18 +1,24 @@
 // HomeAbout //
-import React from 'react';
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../context/LanguageContext';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import '../HomeAbout/HomeAbout.scss';
 import Introduction from '../../components/Introduction/Introduction';
 
 function HomeAbout() {
+  const { language, translations } = useContext(LanguageContext);
+  const t = translations[language].homeAbout;
+
   return (
     <HelmetProvider>
-              <Helmet>
-          <title>Home & About | Oleksandr Pryvalov</title>
-        </Helmet>
+      <Helmet>
+        <title>Home & About | Oleksandr Pryvalov</title>
+      </Helmet>
       <section className='home-about-container'>
-        <h1 className='menu-section-title'>Home/About</h1>
+        <h1 className='menu-section-title'>{t.title}</h1>
+        <div className='introduction-wrapper'>
         <Introduction />
+        </div>
       </section>
     </HelmetProvider>
   );
